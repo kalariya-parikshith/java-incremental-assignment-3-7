8d40228f46d0be1eb292df59b3a73f5ef681c434
kalariya-parikshith <k.parikshith93@gmail.com>
2019-02-20 15:52:29 +0530

    Update CheckPattern.java

--- IncrementalAssignment5/CheckPattern.java
+++ IncrementalAssignment5/CheckPattern.java
@@ -1,40 +1,41 @@
+package com.parikshith.assignment.main;
+import com.sun.istack.internal.NotNull;
+
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.logging.Logger;
 
-class CheckPattern {
-	private static Logger logger = Logger.getLogger("checkPattern log");
-	private static final Pattern pattern = Pattern.compile("^[A-Z].*[.]$");
+class Main {
+    private static Logger logger = Logger.getLogger("checkPattern log");
+    private static final Pattern pattern = Pattern.compile("^[A-Z].*[.]$");
 
-	/**
-	 * checks if the given sentence matches with required pattern
-	 * 
-	 * @param 	sentence input string to be checked against pattern
-	 * 
-	 * @throws 	NullPointerException if the given sentence in the argument
-	 * 			is null.
-	 * @return 	true 	if sentence matches with the pattern
-	 * 			false	else
-	 */
-	public static boolean isPatternMatched(String sentence) throws NullPointerException {
-		Matcher matcher = pattern.matcher(sentence);
-		return matcher.matches();
-	}
+    /**
+     * checks if the given sentence matches with required pattern
+     *
+     * @param 	sentence input non null string to be checked against pattern
+     *
+     * @return 	true 	if sentence matches with the pattern
+     * 			false	else
+     */
+    private static boolean isPatternMatched(@NotNull String sentence) {
+        Matcher matcher = pattern.matcher(sentence);
+        return matcher.matches();
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String[] testSentences = {"Ab.", "ab.", "Abc", "abc.", "a.", "A.", "aB.a", ".", "", null};
+        String[] testSentences = {"Ab.", "ab.", "Abc", "abc.", "a.", "A.", "aB.a", ".", "", null};
 
-		for(String sentence: testSentences) {
-			try {
-				if(isPatternMatched(sentence)) {
-					logger.info("'"+sentence+"' matches as per the pattern");
-				} else {
-					logger.info("'"+sentence+"' doesn't match as per the pattern");
-				}
-			} catch(NullPointerException e) {
-				logger.info("null was passed as argument to isPatternMatched method");
-			}
-		}
-	}
+        for(String sentence: testSentences) {
+            if(sentence == null){
+                logger.info("sentence cannot be null");
+            } else {
+                if (isPatternMatched(sentence)) {
+                    logger.info("'" + sentence + "' matches as per the pattern");
+                } else {
+                    logger.info("'" + sentence + "' doesn't match as per the pattern");
+                }
+            }
+        }
+    }
 }
